16050cc6bed671affe10b2eeb99202bf397d646b
Daniel Perez <nukenabber834159672@gmail.com>
2024-09-24 13:45:20 -0400

    posicionamiento de togger cancion en la parte de arriba y funcionamiento de play y pause + cambio de color

--- script.js
+++ script.js
@@ -29,6 +29,17 @@ window.onload = function() {
     document.body.classList.remove('body-sin-scroll'); // Habilitar el desplazamiento del cuerpo
     cancion.play(); // Reproducir la canción
   }
+
+  // Función para pausar/reproducir la canción
+function toggleReproducirPausar() {
+  if (cancion.paused) {
+    cancion.play(); // Si está pausada, reproducir la canción
+    // document.getElementById('botonMusica').textContent = 'Pausar';
+  } else {
+    cancion.pause(); // Si se está reproduciendo, pausar la canción
+    // document.getElementById('botonMusica').textContent = 'Reproducir';
+  }
+}
   var flotante = document.getElementById('botonMusica')
   // Referencia al elemento de audio
 var cancion = document.getElementById('cancion');
